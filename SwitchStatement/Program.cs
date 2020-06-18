d191d245356eb8ecec92fb692007df2ef048f515
Jada <jada.fagan@gmail.com>
2020-06-18 10:15:16 -0400

    Add project files.

---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter State Code : ");
+            var statecode = Console.ReadLine();
+            var stcd = statecode.ToLower();
+            //if(statecode == "MT" || statecode == "mt")
+            //    Console.WriteLine($"State is Montana");
+
+            // .toLower makes it case insensative but have to input variable lowercase
+            switch(stcd)
+            {
+                case "mt": Console.WriteLine($"State is Montana"); break;
+                case "az": Console.WriteLine($"State is Arizona"); break;
+                case "oh": Console.WriteLine($"State is Ohio"); break;
+                default: Console.WriteLine($"State code is not found"); break;
+            }
+        }
+    }
+}
